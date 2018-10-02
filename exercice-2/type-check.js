cb764f88604966c5f8a9ca35aa352969ce20b6f7
rudyLantoarijaona <rudy.lantoarijaona@gmail.com>
2018-10-02 19:01:22 +0200

    [TEST] exercice-2

---
+++ exercice-2/type-check.js
@@ -0,0 +1,18 @@
+function type_check_v1(args1,args2){
+   /*  if(typeof args2  === 'Array' || typeof args2 === 'null')
+    {
+        args2 = 'object';
+    }     */
+    return (typeof(args1)  === args2);
+}
+
+function type_check_v2(object,val){
+
+}
+
+function type_check(){
+
+}
+
+/*console.log(type_check_v1(null,'null'));
+console.log(type_check_v1([],'Array'));*/
