2d7f70a97ff542ace5330d109d5c6b1b3622dee2
rudyLantoarijaona <rudy.lantoarijaona@gmail.com>
2018-12-16 15:34:39 +0100

    [TEST] exercise-4

---
+++ exercice-4/mario-kart.js
@@ -0,0 +1,26 @@
+function MyPilote(state,origin){
+    state = this.state
+    origin = this.origin
+}
+
+var pilote = MyPilote();
+
+
+
+
+function MyVoiture(name,velocity,onFire){
+    name = this.name
+    velocity = this.velocity
+    onFire = this.onFire
+}
+
+var voiture = MyVoiture();
+
+
+
+
+function MyArmes(type){
+    type = this.type
+}
+
+var arme = MyArmes();
